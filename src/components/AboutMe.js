2f830845e3505a5bff0db0216aa146dc497872cd
g1isgone <ji_won_chung@brown.edu>
2024-08-30 23:54:35 -0400

    (1) new year, new resuME

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,14 +11,14 @@ export default function AboutMe() {
     <Typography align="left"  variant="subtitle1" sx={{fontWeight:'light'}}>
       Hi, I'm Ji Won and I'm a {pageData.aboutMe.occupation} at Brown University.
       I'm advised by <Link href={pageData.aboutMe.advisor1.ref} color="#53599A" underline="hover"> {pageData.aboutMe.advisor1.name}</Link> in the <Link href={pageData.aboutMe.advisor1.labRef} color="#53599A" underline="hover"> {pageData.aboutMe.advisor1.labName}. </Link> 
-      I also run the <Link href="https://sites.google.com/brown.edu/gwics" color="#53599A" underline="hover">GWiCS+</Link> group at Brown and have somehow become a plant person <span>&#8212;</span> artificial and of the sort.
       <p></p>
-      My primary research interests are in <BoldText txt="creating long-term, human-centric systems that adapt to changing human behaviors."/> 
+      My primary research interests are in <BoldText txt="creating long-term, human-centric systems that adapt to natural human behaviors."/> 
       People naturally change over time, but it's an aspect we often overlook. 
       So how can systems reflect that and what can we learn from our ever-changing selves?
       I've investigated this in the context of sleep and mixed reality so far, but more to come!
       <p></p>
-      That's all for now, but please feel free to email me. Will work for coffee :) 
+      If you're wondering what the green blob is... that's a platypus next to some animated plants. Yes, I've sort of become a plant person <span>&#8212;</span> artificial and of the sort.
+      That's all for now, but please feel free to email me, details in resume. Will work for coffee :) 
       <p></p> 
      </Typography>
     </div>
